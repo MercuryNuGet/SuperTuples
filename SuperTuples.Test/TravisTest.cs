ff4fd8b2850a25889dcf5d89d3b266753fc29728
Alan Evans <thealanevans@googlemail.com>
2016-02-04 21:35:39 +0000

    Test project

---
+++ SuperTuples.Test/TravisTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace SuperTuples.Test
+{
+    [TestFixture]
+    public sealed class TravisTest
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.Pass();
+        }
+    }
+}
