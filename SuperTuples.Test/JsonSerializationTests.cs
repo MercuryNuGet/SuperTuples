328f1cc8fc47a21fd21af41b584fc7d07a44994b
Alan Evans <thealanevans@googlemail.com>
2016-03-29 06:43:03 +0100

    Json serialization tests

---
+++ SuperTuples.Test/JsonSerializationTests.cs
@@ -0,0 +1,31 @@
+﻿using Mercury;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace SuperTuples.Test
+{
+    public sealed class JsonSerializationTests : MercurySuite
+    {
+        protected override void Specifications()
+        {
+            Specs += "When serializing #1 to json"
+                .ArrangeNull()
+                .With(new Person("Alan", "Evans", 35), "{\"FirstName\":\"Alan\",\"LastName\":\"Evans\",\"Age\":35}")
+                .With(new Person("John", "Doe", 45), "{\"FirstName\":\"John\",\"LastName\":\"Doe\",\"Age\":45}")
+                .Act((_, p, e) => JsonConvert.SerializeObject(p))
+                .Assert("expect json to not include protected members", (r, expected) => Assert.AreEqual(expected, r));
+        }
+
+        public class Person : Suple<string, string, int>
+        {
+            public Person(string firstName, string lastName, int age)
+                : base(firstName, lastName, age)
+            {
+            }
+
+            public string FirstName => Item1;
+            public string LastName => Item2;
+            public int Age => Item3;
+        }
+    }
+}
