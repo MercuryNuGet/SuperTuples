2fb0e6d4fda9e296acc3f737d5d73ae4be8a2059
Alan Evans <thealanevans@googlemail.com>
2016-03-29 12:45:25 +0100

    Unchecked block around calculate of hashcode

---
+++ SuperTuples.Test/UncheckedBlockTests.cs
@@ -0,0 +1,37 @@
+﻿using Mercury;
+using NUnit.Framework;
+
+namespace SuperTuples.Test
+{
+    public sealed class UncheckedBlockTests : MercurySuite
+    {
+        protected override void Specifications()
+        {
+            const int expectedHash = 1062386622;
+
+            Specs += "When calculating hash, uses an unchecked environment"
+                .Arrange(() => new TrippleString("a", "b", "c"))
+                .Act(sut => sut.GetHashCode())
+                .Assert(r => Assert.AreEqual(expectedHash, r));
+
+            Specs += "When pre-calculating hash, uses an unchecked environment"
+                .Arrange(() => new TrippleStringCached("a", "b", "c"))
+                .Act(sut => sut.GetHashCode())
+                .Assert(r => Assert.AreEqual(expectedHash, r));
+        }
+
+        public class TrippleString : Suple<string, string, string>
+        {
+            public TrippleString(string a, string b, string c) : base(a, b, c)
+            {
+            }
+        }
+
+        public class TrippleStringCached : Suple<string, string, string>
+        {
+            public TrippleStringCached(string a, string b, string c) : base(a, b, c, SupleHash.Cached)
+            {
+            }
+        }
+    }
+}
